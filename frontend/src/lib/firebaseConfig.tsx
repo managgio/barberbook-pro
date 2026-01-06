e104b181b1dd42a30df24fd656612e1948ab7986
Noddeio <executive.nodde@gmail.com>
2026-01-06 21:51:10 +0100

    abstraction of the firebase variables

--- frontend/src/lib/firebaseConfig.tsx
+++ frontend/src/lib/firebaseConfig.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAAXSNp-Hjmqyj4nGkDLTKJUXeCuIrNdYA',
-  authDomain: 'barberbookpro-local.firebaseapp.com',
-  projectId: 'barberbookpro-local',
-  storageBucket: 'barberbookpro-local.firebasestorage.app',
-  messagingSenderId: '905277075740',
-  appId: '1:905277075740:web:d4b20b24e4c852b2e30ad9',
-  measurementId: 'G-DBSTP3VLH0',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
